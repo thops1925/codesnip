e6cf73c0bcbba438b5868f96acf57e87d3af6577
ThopsD <74445064+thops1925@users.noreply.github.com>
2023-06-15 17:03:36 +0800

    fetc

--- app/(server)/api/prompt/route.ts
+++ app/(server)/api/prompt/route.ts
@@ -1,14 +1,20 @@
-import Prompt from '@models/prompt';
-import { connectToDB } from '@utils/database';
-import { NextResponse } from 'next/server';
+import Prompt from "@models/prompt";
+import { connectToDB } from "@utils/database";
 
 export async function GET(request: Request) {
     try {
-        await connectToDB()
-        const prompts = await Prompt.find({}).populate('creator')
-        if (!prompts) return new Response('no connection', { status: 404 });
-        return NextResponse.json(prompts, { status: 200 });
+        await connectToDB(); // Connect to the database
+
+        const prompts = await Prompt.find({}).populate('creator'); // Fetch all prompts and populate the 'creator' field
+
+        if (!prompts) {
+            return new Response('No prompts found', { status: 404 });
+        }
+
+        return new Response(JSON.stringify(prompts), { status: 200, headers: { 'Content-Type': 'application/json' } });
     } catch (error) {
-        return new Response("Failed to fetch all prompts", { status: 500 })
+        console.log(error); // Log the error for debugging purposes
+
+        return new Response('Failed to fetch all prompts', { status: 500 });
     }
-} 
+}
