bc4e6691068f11607bc3708a29f763b8fd34b59c
ThopsD <74445064+thops1925@users.noreply.github.com>
2023-06-15 19:16:11 +0800

    las

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 'use client';
 import React, { useEffect, useState } from 'react';
 import Prompt from './Prompt';
@@ -6,35 +7,32 @@ import { PromptList } from './PromptList ';
 const Feed = () => {
 	const [searchText, setSearchText] = useState('');
 	const [searchData, setSearchData] = useState([]);
-	const [postData, setData] = useState([]);
-	const [isLoading, setIsLoading] = useState(true);
-
-	const getAllData = async () => {
-		const res = await fetch(`/api/prompt`);
-		const data = await res.json();
-		setData(data.reverse());
-		setIsLoading(false);
-	};
+	const [data, setData] = useState([]);
 
 	useEffect(() => {
+		const getAllData = async () => {
+			const res = await fetch(`/api/prompt`);
+			const data = await res.json();
+			setData(data.reverse());
+		};
 		getAllData();
 	}, []);
+
+	useEffect(() => {
+		filterSearch(searchText);
+	}, [searchText]);
 
 	const handleSearch = (e: any) => {
 		e.preventDefault();
 		setSearchText(e.target.value);
+		filterSearch(searchText);
 	};
 
 	const filterSearch = (searchText: string) => {
 		const regex = new RegExp(searchText, 'i'); // 'i' flag for case-insensitive search
-		const res: any = postData.filter((item: Post) => regex.test(item.creator.username) || regex.test(item.prompt) || regex.test(item.tag));
+		const res: any = data.filter((item: Post) => regex.test(item.creator.username) || regex.test(item.prompt) || regex.test(item.tag));
 		setSearchData(res);
 	};
-
-	useEffect(() => {
-		filterSearch(searchText);
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [searchText]);
 
 	return (
 		<section className='container sm:container lg:container md:container flex justify-center items-center flex-col'>
@@ -48,7 +46,7 @@ const Feed = () => {
 					className='block w-full my-6 rounded-md border border-gray-200 bg-white py-2.5 font-satoshi pl-5 pr-12 text-sm shadow-lg font-medium focus:border-black focus:outline-none focus:ring-0'
 				/>
 			</form>
-			{searchText ? <PromptList data={searchData} /> : <PromptList data={postData} />}
+			{searchText ? <PromptList data={searchData} /> : <PromptList data={data} />}
 		</section>
 	);
 };
