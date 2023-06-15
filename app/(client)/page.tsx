8b5dbfb96068687a5a87c5e3c37503a2fbafcdc7
ThopsD <74445064+thops1925@users.noreply.github.com>
2023-06-15 17:18:37 +0800

    fa

--- app/(client)/page.tsx
+++ app/(client)/page.tsx
@@ -7,35 +7,28 @@ import { useEffect, useState } from 'react';
 
 const getAllData = async () => {
 	const res = await fetch('/api/prompt');
-
 	if (res.status === 304) {
 		// Resource not modified, no need to update the data
-		return null;
+		return;
 	}
-
 	return res.json();
 };
 
 const Home = () => {
-	const [postData, setPostData] = useState([]);
-	const [dataVersion, setDataVersion] = useState(0); // State to track the data version
+	const [postData, setData] = useState([]);
 
 	useEffect(() => {
+		const controller = new AbortController();
 		const fetchAll = async () => {
 			const data = await getAllData();
-
-			if (data) {
-				setPostData(data.reverse());
-				setDataVersion((prevVersion) => prevVersion + 1); // Update data version
-			}
+			setData(data.reverse());
 		};
-
-		const intervalId = setInterval(fetchAll, 5000); // Fetch data every 5 seconds
-
+		fetchAll();
 		return () => {
-			clearInterval(intervalId); // Clear the interval when the component unmounts
+			// cancel the request before component unmounts
+			controller.abort();
 		};
-	}, []); // Empty dependency array to run the effect only once
+	}, []);
 
 	return (
 		<section className='flex flex-col items-center justify-center w-full'>
@@ -43,9 +36,8 @@ const Home = () => {
 				<h1 className='text-normal text-center font-mono tracking-wider text-gray-800 '>{desc}</h1>
 				<Image src={logo} alt='logo' className='object-contain blur-0' blurDataURL='data:...' placeholder='blur' />
 			</div>
-			<Feed postData={postData} dataVersion={dataVersion} /> {/* Pass data version as prop */}
+			<Feed postData={postData} />
 		</section>
 	);
 };
-
 export default Home;
