76c57eea23011769f25c1d7700e7fb46d082ad73
ThopsD <74445064+thops1925@users.noreply.github.com>
2023-06-15 18:04:01 +0800

    h

--- components/Profile.tsx
+++ components/Profile.tsx
@@ -12,7 +12,7 @@ const Profile = ({ name, desc, data, handleEdit, handleDelete }: Props) => {
 	return (
 		<div className='container sm:container lg:container md:container flex justify-center items-center flex-col my-4'>
 			<div className='flex justify-start items-start flex-col space-y-3'>
-				<span className='text-2xl font-bold mx-4'>{name.slice(0, 6)}</span>
+				<span className='text-2xl font-bold mx-4'>{name}</span>
 				<h1 className='font-mono text-sm font-normal mx-4 lg:w-1/2'>{desc}</h1>
 			</div>
 
