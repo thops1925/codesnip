40264c4863ddaaf0c05dff0e1c3fef6b71dcd954
ThopsD <74445064+thops1925@users.noreply.github.com>
2023-06-18 22:35:30 +0800

    time

--- app/(client)/create-prompt/page.tsx
+++ app/(client)/create-prompt/page.tsx
@@ -4,7 +4,6 @@ import { useState } from 'react';
 import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
 import Form from '@components/Form';
-import { useQueryClient } from '@tanstack/react-query';
 
 const CreatePrompt = () => {
 	const router = useRouter();
